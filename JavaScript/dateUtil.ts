7284f18fe9e84c88a517ffe013d8b7e0e22f0368
Peyman Ekhtiar <peyman.ekhtiar@decsoft.com.pl>
2019-02-07 10:09:03 +0100

    create dateUtil.ts

---
+++ JavaScript/dateUtil.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DateUtil {
+
+constructor() {  }
+
+
+public calcDate(date1: Date,date2: Date) {
+    var diff = Math.floor(date2.getTime() - date1.getTime());
+    var day = 1000 * 60 * 60 * 24;        
+
+    var days = Math.floor(diff/day);
+    var years = Math.floor(days/365);
+    var months = Math.floor((days%365)/30);
+
+    var message = "";
+    message += " from ";
+    if (years > 1)
+    	message += years + " years and ";
+    else 
+    	message += years + " year and ";
+    if (months > 1)
+    	message += months + " months ";
+   	else
+   		message += months + " month ";
+
+    return message
+  }
+
+}
